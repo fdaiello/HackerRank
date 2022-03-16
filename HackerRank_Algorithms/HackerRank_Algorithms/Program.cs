0739f8009f05f6f27da64ded12a11edff0e7f3d8
Felipe Daiello <fdaiello@misterpostman.com.br>
2022-03-16 10:57:54 -0300

    SuperReducedString

---
+++ HackerRank_Algorithms/HackerRank_Algorithms/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+namespace HackerRank_Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestSuperReducedString();
+        }
+        #region SuperReducedString
+        public static void TestSuperReducedString()
+        {
+            Console.WriteLine(SuperReducedString("baab"));
+            Console.WriteLine("Expected: -empty string-");
+        }
+        public static string SuperReducedString(string s)
+        {
+            var cList = s.ToCharArray().ToList();
+
+            int p;
+            bool removed;
+
+            do
+            {
+                removed = false;
+                p = 0;
+                while (p < cList.Count - 1)
+                {
+                    if (cList[p] == cList[p + 1])
+                    {
+                        cList.RemoveAt(p);
+                        cList.RemoveAt(p);
+                        removed = true;
+                    }
+                    else
+                        p++;
+                }
+            } while (removed);
+
+            if (cList.Count > 0)
+                return new String(cList.ToArray());
+            else
+                return "Empty String";
+        }
+        #endregion
+    }
+}
